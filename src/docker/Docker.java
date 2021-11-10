7819db524b51b39ba03d1350196ade3a54e6e75f
PrayogaBismaPutraSeza <prayogabisma1@gmail.com>
2021-11-10 20:39:31 +0700

    final

---
+++ src/docker/Docker.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package docker;
+
+/**
+ *
+ * @author Yozora
+ */
+public class Docker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Nama    : Prayoga Bisma P.S");
+        System.out.println("NIM     : 1931710022");
+        System.out.println("Absen   : 18");
+    }
+    
+}
